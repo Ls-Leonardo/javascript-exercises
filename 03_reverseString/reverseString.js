d4fbe570a51e1e46e977a0170e2b0b4c4b50aaa5
Leonardo Sudré <165829938+Ls-Leonardo@users.noreply.github.com>
2024-12-16 18:47:25 -0300

    Completed all seven initial exercises from Odin

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if(string.length === 0){
+        return ""
+    }
 
+    let reverse;
+    reverse = string.split("")
+
+    reverse = reverse.reverse()
+
+    return reverse.join("")
+    
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let c = 0
+ 
+  c = 5/9 * (fahrenheit - 32)
+  
+  return parseFloat(c.toFixed(1)) // converts to number
+
+
+ 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+let f = 0
+
+f = (9/5 * celsius) + 32
+return parseFloat(f.toFixed(1))
+
 };
+
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = {
