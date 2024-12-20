a010570691ce2ef7c4d72d47ff833557fd147ddc
Leonardo Sudré <165829938+Ls-Leonardo@users.noreply.github.com>
2024-12-20 20:59:22 -0300

    The exercises for this module have been completed.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanedStr = ""
+
+    for(let i = 0; i < str.length; i++){
+        const char = str[i].toLowerCase()
+
+        if((char >= "a" && char <= "z") || (char >= '0' && char <= '9') ){
+            cleanedStr += char
+        }
+
+    }
+
+    const reversedStr = cleanedStr.split("").reverse().join("")
+    return cleanedStr === reversedStr
+
+   
 
 };
 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current)=> {
+        const currentAge = current.yearOfDeath
+        ? current.yearOfDeath - current.yearOfBirth
+        : new Date().getFullYear() - current.yearOfBirth;
 
+        const oldestAge = oldest.yearOfDeath
+        ? oldest.yearOfDeath - oldest.yearOfBirth
+        : new Date().getFullYear() - oldest.yearOfBirth;
+
+        // Retorna a pessoa mais velha
+        return currentAge > oldestAge ? current : oldest;
+    })
 };
 
 // Do not edit below this line
